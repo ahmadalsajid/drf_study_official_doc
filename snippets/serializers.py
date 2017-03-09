32d9ca048d3ad5643cf932f02d9a7037f0408db5
ahmadalsajid <aa@inov.io>
2017-03-09 13:01:16 +0600

    correction
    slightly updated

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -16,4 +16,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'password')
+        fields = ('id', 'username', 'password', 'snippets')
